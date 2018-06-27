a0f2f8df7dac40c62b7904f3ac4a42027639aa90
Vaughn Walters <vwalters@paramoredigital.com>
2018-06-27 13:03:10 -0500

    removing hardcoded data, and replaing with dynamic blog content on homepage

--- includes/navigation.php
+++ includes/navigation.php
@@ -28,15 +28,6 @@
 
                 ?>
 
-                <!--                <li>-->
-<!--                    <a href="#">About</a>-->
-<!--                </li>-->
-<!--                <li>-->
-<!--                    <a href="#">Services</a>-->
-<!--                </li>-->
-<!--                <li>-->
-<!--                    <a href="#">Contact</a>-->
-<!--                </li>-->
             </ul>
         </div>
         <!-- /.navbar-collapse -->
