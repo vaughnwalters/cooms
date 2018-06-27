6b3491aef390e24f34fa43820aa112fb9e9b249c
Vaughn Walters <vaughnwalters@gmail.com>
2018-06-27 15:03:28 -0500

    blog categories displaying dynamically

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,33 +1,5 @@
 <div class="col-md-4">
 
-<?php
-//
-//    if(isset($_POST['submit'])) {
-//        $search = $_POST['search'];
-//
-//        $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
-//        $search_query = mysqli_query($connection, $query);
-//
-//        if (!$search_query) {
-//            die("QUERY FAILED" . mysqli_error($connection));
-//        }
-//
-//        $count = mysqli_num_rows($search_query);
-//
-//        if($count == 0) {
-//            echo "NO REZULTH";
-//        } else {
-//            echo "SUM RETHULTH";
-//        }
-//
-//    }
-//
-//?>
-
-
-
-
-
 
     <!-- Blog Search Well -->
     <div class="well">
@@ -49,24 +21,34 @@
 
 
 
-
-
-
-
     <!-- Blog Categories Well -->
+
+    <?php
+
+        $query = "SELECT * FROM categories";
+        $select_categories_sidebar = mysqli_query($connection, $query);
+
+
+    ?>
+
     <div class="well">
         <h4>Blog Categories</h4>
         <div class="row">
             <div class="col-lg-6">
                 <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
+
+                    <?php
+
+                    while($row = mysqli_fetch_assoc($select_categories_sidebar)) {
+                        $cat_title = $row['cat_title'];
+
+                        echo "<li><a href='#'>{$cat_title}</a></li>";
+
+                    }
+
+
+                    ?>
+
                 </ul>
             </div>
             <!-- /.col-lg-6 -->
