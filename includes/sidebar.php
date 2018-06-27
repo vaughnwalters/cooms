c3a37cb27a9d2749c0b300d7bef495cb75cf4794
Vaughn Walters <vwalters@paramoredigital.com>
2018-06-27 14:04:17 -0500

    correctly querying DB for post tags to and returning whether it exists or not

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,7 +3,23 @@
 <?php
 
     if(isset($_POST['submit'])) {
-        echo $search = $_POST['search'];
+        $search = $_POST['search'];
+
+        $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
+        $search_query = mysqli_query($connection, $query);
+
+        if (!$search_query) {
+            die("QUERY FAILED" . mysqli_error($connection));
+        }
+
+        $count = mysqli_num_rows($search_query);
+
+        if($count == 0) {
+            echo "NO REZULTH";
+        } else {
+            echo "SUM RETHULTH";
+        }
+
     }
 
 ?>
