1b12f713df82c433539171300811e276368325bf
Vaughn Walters <vaughnwalters@gmail.com>
2018-06-28 13:53:41 -0500

    refactoring some functions to functions.php file in admin

--- admin/categories.php
+++ admin/categories.php
@@ -33,7 +33,7 @@
 
                         </form>
 
-                    <?php
+                    <?php //UPDATE AND INCLUDE QUERY
 
                     if(isset($_GET['edit'])) {
                         $cat_id = $_GET['edit'];
@@ -56,38 +56,9 @@
                             </thead>
                             <tbody>
 
+                            <?php findAllCategories(); ?>
 
-
-
-                            <?php // FIND ALL CATEGORIES QUERY
-
-                            $query = "SELECT * FROM categories";
-                            $select_categories = mysqli_query($connection, $query);
-
-                            while($row = mysqli_fetch_assoc($select_categories)) {
-                                $cat_id = $row['cat_id'];
-                                $cat_title = $row['cat_title'];
-                                echo "<tr>";
-                                echo "<td>{$cat_id}</td>";
-                                echo "<td>{$cat_title}</td>";
-                                echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-                                echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-                                echo "</tr>";
-
-                            }
-
-                            ?>
-
-                            <?php // DELETE QUERY
-                                if(isset($_GET['delete'])) {
-                                    $the_cat_id = $_GET['delete'];
-                                    $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
-                                    $delete_query = mysqli_query($connection, $query);
-//                                  this header function will refresh the page
-                                    header("Location: categories.php");
-                                }
-
-                            ?>
+                            <?php delete_categories(); ?>
 
                             </tbody>
                         </table>
--- admin/functions.php
+++ admin/functions.php
@@ -25,4 +25,38 @@ function insert_categories(){
     }
 
 }
+
+function findAllCategories() {
+
+    global $connection;
+
+    $query = "SELECT * FROM categories";
+    $select_categories = mysqli_query($connection, $query);
+
+    while($row = mysqli_fetch_assoc($select_categories)) {
+        $cat_id = $row['cat_id'];
+        $cat_title = $row['cat_title'];
+        echo "<tr>";
+        echo "<td>{$cat_id}</td>";
+        echo "<td>{$cat_title}</td>";
+        echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+        echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
+        echo "</tr>";
+
+    }
+}
+
+function delete_categories() {
+
+    global $connection;
+    if(isset($_GET['delete'])) {
+        $the_cat_id = $_GET['delete'];
+        $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
+        $delete_query = mysqli_query($connection, $query);
+//      this header function will refresh the page
+        header("Location: categories.php");
+    }
+}
+
+
 ?>
