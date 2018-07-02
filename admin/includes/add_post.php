b5b12e20181cb1fa8126aa06012f8da79f7825e5
Vaughn Walters <vaughnwalters@gmail.com>
2018-07-02 12:21:19 -0500

    submiting blog post including image

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,8 +1,25 @@
 <?php
 
 if(isset($_POST['create_post'])) {
-    echo $_POST['title'];
+
+    $post_title = $_POST['title'];
+    $post_author = $_POST['author'];
+    $post_category_id = $_POST['post_category_id'];
+    $post_status = $_POST['post_status'];
+
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+
+    $post_tags = $_POST['post_tags'];
+    $post_content = $_POST['post_content'];
+    $post_date = date('d-m-y');
+    $post_comment_count = 4;
+
+//    this will take the image that is placed into the server temporarily when a file is selected, and move it into the database on submit
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
 }
+
 
 ?>
 
