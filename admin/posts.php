1d11c7890d56bd18f950226943bd1f6e7d275a0c
Vaughn Walters <vaughnwalters@gmail.com>
2018-07-02 10:51:25 -0500

    posts being dynamically added to the admin/posts page

--- admin/posts.php
+++ admin/posts.php
@@ -33,7 +33,50 @@
                         </thead>
 
                         <tbody>
-                        <tr>
+
+                           <?php
+
+                           $query = "SELECT * FROM posts";
+                           $select_posts = mysqli_query($connection, $query);
+
+                           while($row = mysqli_fetch_assoc($select_posts)) {
+                               $post_id = $row['post_id'];
+                               $post_author= $row['post_author'];
+                               $post_title = $row['post_title'];
+                               $post_category_id = $row['post_category_id'];
+                               $post_status = $row['post_status'];
+                               $post_image = $row['post_image'];
+                               $post_tags = $row['post_tags'];
+                               $post_comment_count = $row['post_comment_count'];
+                               $post_date = $row['post_date'];
+
+                               echo "<tr>";
+                               echo "<td>$post_id</td>";
+                               echo "<td>$post_author</td>";
+                               echo "<td>$post_title</td>";
+                               echo "<td>$post_category_id</td>";
+                               echo "<td>$post_status</td>";
+                               echo "<td>$post_image</td>";
+                               echo "<td>$post_tags</td>";
+                               echo "<td>$post_comment_count</td>";
+                               echo "<td>$post_date</td>";
+
+
+                               echo "</tr>";
+
+
+
+
+                           }
+
+                           ?>
+
+
+
+
+
+
+
                             <td>10</td>
                             <td>VDUBZ</td>
                             <td>bootstrap framework</td>
@@ -43,7 +86,6 @@
                             <td>tags</td>
                             <td>comments</td>
                             <td>date</td>
-                        </tr>
                         </table>
                     </tbody>
 
