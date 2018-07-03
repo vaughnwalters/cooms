29cdd77c22fc43aabf97cbcbc33d97a23eee2350
Vaughn Walters <vaughnwalters@gmail.com>
2018-07-03 12:57:09 -0500

    dynamic content being pulled into edit blog posts fields

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -42,6 +42,8 @@
         echo "<td>$post_tags</td>";
         echo "<td>$post_comment_count</td>";
         echo "<td>$post_date</td>";
+//        passing two params here, source to go to the edit_post page and p_id to to to the specific entry to be edited
+        echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
         echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
 
         echo "</tr>";
