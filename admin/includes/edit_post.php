6a591eedd68c174b35f366fc73dbdef56c8554b2
Vaughn Walters <vaughnwalters@gmail.com>
2018-07-03 14:03:58 -0500

    working through update functionality in blog - images still not updating correctly

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -24,6 +24,47 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
 }
 
+//grabbing all values from $_POST superglobal
+if(isset($_POST['update_post'])) {
+    $post_author = $_POST['author'];
+    $post_title = $_POST['title'];
+    $post_category = $_POST['post_category'];
+    $post_status = $_POST['post_status'];
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+    $post_content = $_POST['post_content'];
+    $post_tags = $_POST['post_tags'];
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    if(empty($post_image)) {
+        $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
+        $select_image = mysqli_query($connection, $query);
+
+        while($row = mysqli_fetch_array($select_image)) {
+            $post_image = $row['post_image'];
+        }
+
+
+    }
+
+
+    $query = "UPDATE posts SET ";
+    $query .= "post_title  = '{$post_title}', ";
+    $query .= "post_category_id  = '{$post_category_id}', ";
+    $query .= "post_date   = now(), ";
+    $query .= "post_author = '{$post_author}', ";
+    $query .= "post_status = '{$post_status}', ";
+    $query .= "post_tags   = '{$post_tags}', ";
+    $query .= "post_content  = '{$post_content}' ";
+    $query .= "WHERE post_id = {$the_post_id} ";
+
+    $update_post = mysqli_query($connection, $query);
+
+    confirmQuery($update_post);
+
+}
+
 
 ?>
 
@@ -44,10 +85,10 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
             <?php
 
-            $query = "SELECT * FROM categories";
+            $query = "SELECT * FROM categories ";
             $select_categories = mysqli_query($connection, $query);
 
-//            confirmQuery($select_categories);
+            confirmQuery($select_categories);
 
             while($row = mysqli_fetch_assoc($select_categories)) {
                 $cat_id = $row['cat_id'];
@@ -85,6 +126,7 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
     <div class="form-group">
         <img width="100" src="../images/<?php echo $post_image; ?>" alt="">
+        <input type="file" name="image">
     </div>
 
     <div class="form-group">
@@ -102,7 +144,7 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
     </div>
 
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+        <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
     </div>
 
 
