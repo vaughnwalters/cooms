3b96e2b26bea792dfe481622ed50d0a89184cd25
Vaughn Walters <vaughnwalters@gmail.com>
2018-07-03 12:35:42 -0500

    adding delete functionality for blog posts

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -42,7 +42,7 @@
         echo "<td>$post_tags</td>";
         echo "<td>$post_comment_count</td>";
         echo "<td>$post_date</td>";
-
+        echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
 
         echo "</tr>";
 
@@ -70,4 +70,16 @@
     <td>date</td>
 </table>
 </tbody>
+
+<?php
+
+if(isset($_GET['delete'])) {
+
+    $the_post_id = $_GET['delete'];
+    $query = "DELETE FROM posts WHERE post_id = {$the_post_id} ";
+    $delete_query = mysqli_query($connection, $query);
+    header("Location: posts.php");
+}
+
+?>
 
