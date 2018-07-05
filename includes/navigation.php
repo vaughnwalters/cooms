077aca76ec1d51b9323c0017c836783ea1477081
Vaughn Walters <vaughnwalters@gmail.com>
2018-07-05 14:40:46 -0500

    creating comments template from posts template

--- includes/navigation.php
+++ includes/navigation.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            COOMS
+            <a class="navbar-brand" href="index.php">COOMS</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
