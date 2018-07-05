31f0dfbeb49bfcaf8934f559b70d3cc73fa0af75
Vaughn Walters <vaughnwalters@gmail.com>
2018-07-05 18:10:08 -0500

    comments displaying correctly on page once approved in back end

--- post.php
+++ post.php
@@ -124,11 +124,6 @@
                         <textarea name="comment_content" class="form-control" rows="3"></textarea>
                     </div>
 
-
-
-
-
-
                     <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
                 </form>
             </div>
@@ -136,6 +131,25 @@
             <hr>
 
             <!-- Posted Comments -->
+            <?php
+
+            $the_post_id = $_GET['p_id'];
+
+            $query = "SELECT * FROM comments WHERE comment_post_id = {$the_post_id} ";
+            $query .= "AND comment_status = 'approved' ";
+            $query .= "ORDER BY comment_id DESC ";
+            $select_comment_query = mysqli_query($connection, $query);
+            if(!$select_comment_query) {
+                die('Query Failed' . mysqli_error($connection));
+            }
+            while ($row = mysqli_fetch_array($select_comment_query)) {
+                $comment_date = $row['comment_date'];
+                $comment_content = $row['comment_content'];
+                $comment_author = $row['comment_author'];
+
+            ?>
+
+
 
             <!-- Comment -->
             <div class="media">
@@ -143,38 +157,21 @@
                     <img class="media-object" src="http://placehold.it/64x64" alt="">
                 </a>
                 <div class="media-body">
-                    <h4 class="media-heading">Start Bootstrap
-                        <small>August 25, 2014 at 9:30 PM</small>
+                    <h4 class="media-heading"><?php echo $comment_author; ?>
+                        <small><?php echo $comment_date; ?></small>
                     </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    <?php echo $comment_content ?>
+
                 </div>
             </div>
 
-            <!-- Comment -->
-            <div class="media">
-                <a class="pull-left" href="#">
-                    <img class="media-object" src="http://placehold.it/64x64" alt="">
-                </a>
-                <div class="media-body">
-                    <h4 class="media-heading">Start Bootstrap
-                        <small>August 25, 2014 at 9:30 PM</small>
-                    </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                    <!-- Nested Comment -->
-                    <div class="media">
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="http://placehold.it/64x64" alt="">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading">Nested Start Bootstrap
-                                <small>August 25, 2014 at 9:30 PM</small>
-                            </h4>
-                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        </div>
-                    </div>
-                    <!-- End Nested Comment -->
-                </div>
-            </div>
+
+
+
+
+
+            <?php } ?>
+
 
 
 
@@ -186,9 +183,9 @@
 
 
         </div>
-
-        <!-- Blog Sidebar Widgets Column -->
-        <?php include "includes/sidebar.php"; ?>
+<!---->
+<!--        <!-- Blog Sidebar Widgets Column -->
+<!--        --><?php //include "includes/sidebar.php"; ?>
 
     </div>
     <!-- /.row -->
