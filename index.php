901aec9338b52a04b65ec804a8eb454d9d048132
Vaughn Walters <vaughnwalters@gmail.com>
2018-07-05 13:52:32 -0500

    can click on blog post on index to go to individual plog post page

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
                 $select_all_posts_query = mysqli_query($connection, $query);
 
                 while($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                    $post_id = $row['post_id'];
                     $post_title = $row['post_title'];
                     $post_author = $row['post_author'];
                     $post_date = $row['post_date'];
@@ -39,7 +40,7 @@
 
                     <!-- First Blog Post -->
                     <h2>
-                        <a href="#"><?php echo $post_title ?></a>
+                        <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
                     </h2>
                     <p class="lead">
                         by <a href="index.php"><?php echo $post_author ?></a>
