549657b634ec4ebc17071abb7b6f5995af839f8e
Vaughn Walters <vaughnwalters@gmail.com>
2018-07-05 19:03:47 -0500

    conditional for if status is published - sucks, needs to be refactored, but works

--- index.php
+++ index.php
@@ -27,6 +27,15 @@
                     $post_date = $row['post_date'];
                     $post_image = $row['post_image'];
                     $post_content = substr($row['post_content'], 0,100);
+                    $post_status = $row['post_status'];
+
+                    if($post_status !== 'published') {
+
+//                        echo "<h1 class='text-center'>NO POSTS SORRY</h1>";
+
+                    } else {
+
+
 
                     ?>
 
@@ -54,7 +63,7 @@
 
                     <hr>
 
-                <?php } ?>
+                <?php } }?>
 
 
             </div>
