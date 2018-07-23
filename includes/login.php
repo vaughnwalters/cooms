7dc5f8aadc3cad9a8fc9d850138ec2c884fd2b13
Vaughn Walters <vaughnwalters@gmail.com>
2018-07-23 12:54:46 -0500

    refactoring login to admin

--- includes/login.php
+++ includes/login.php
@@ -30,17 +30,14 @@ while($row = mysqli_fetch_array($select_user_query)) {
 
 }
 
-if($username !== $db_username && $password !== $db_user_password ) {
-    header("Location: ../index.php");
-} elseif ($username == $db_username && $password == $db_user_password) {
-
+if($username === $db_username && $password === $db_user_password ) {
 //    assigning variable's value to this session
     $_SESSION['username'] = $db_username;
     $_SESSION['firstname'] = $db_user_firstname;
     $_SESSION['lastname'] = $db_user_lastname;
     $_SESSION['user_role'] = $db_user_role;
-
     header( "Location: ../admin");
+
 } else {
     header("Location: ../index.php");
 }
