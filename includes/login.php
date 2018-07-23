59a78df265b1bf5d733a83843dfbd5784135f0af
Vaughn Walters <vaughnwalters@gmail.com>
2018-07-23 11:56:29 -0500

    adding ability to log in to admin panel from homepage

--- includes/login.php
+++ includes/login.php
@@ -1,3 +1,7 @@
+<?php
+ob_start();
+?>
+
 <?php include "db.php"; ?>
 
 <?php
@@ -11,7 +15,7 @@ if(isset($_POST['login'])) {
 $username = mysqli_real_escape_string($connection, $username);
 $password = mysqli_real_escape_string($connection, $password);
 
-$query = "SELECT * FROM users WHERE username = '{$username}'";
+$query = "SELECT * FROM users WHERE username = '{$username}' ";
 $select_user_query = mysqli_query($connection, $query);
 if(!$select_user_query) {
     die("QUERY FAILED" . mysqli_error($connection));
@@ -19,12 +23,22 @@ if(!$select_user_query) {
 
 while($row = mysqli_fetch_array($select_user_query)) {
 
-    echo $db_id = $row['user_id'];
+    $db_user_id = $row['user_id'];
+    $db_username = $row['username'];
+    $db_user_password = $row['user_password'];
+    $db_user_firstname = $row['user_firstname'];
+    $db_usr_lastname = $row['user_lastname'];
+    $db_user_role = $row['user_role'];
 
 }
 
-
-
+if($username !== $db_username && $password !== $db_user_password ) {
+    header("Location: ../index.php");
+} elseif ($username == $db_username && $password == $db_user_password) {
+    header( "Location: ../admin");
+} else {
+    header("Location: ../index.php");
+}
 
 
 ?>
