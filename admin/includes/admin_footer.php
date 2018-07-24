052c6108143d848ef7c137789fb19af98778a0c1
Vaughn Walters <vaughnwalters@gmail.com>
2018-07-24 14:02:19 -0500

    adding WYSIWYG editor to back end

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -7,6 +7,9 @@
 <!-- Bootstrap Core JavaScript -->
 <script src="js/bootstrap.min.js"></script>
 
+<!--adding js file for the WYSIWYG editor in the back end-->
+<script src="scripts.js"></script>
+
 </body>
 
 </html>
