2c7597883d10a33f5ed78454bfec701c317729df
Vaughn Walters <vaughnwalters@gmail.com>
2018-07-25 13:00:36 -0500

    adding social icons into footer :

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,16 @@
 <footer>
     <div class="row">
         <div class="col-lg-12">
-            <p>&copy; <a href="mailto:vaughnwalters@gmail.com?Subject=sup" target="_top">Vaughn Walters</a> <?php echo date("Y"); ?></p>
-
-            <i class="fa fa-github"></i>
-            <i class="fa fa-linkedin"></i>
+            <p>&copy; <a href="mailto:vaughnwalters@gmail.com?Subject=sup" target="_top">Vaughn Walters</a> ® <?php echo date("Y"); ?> ™</p>
 
 
+            <a href="https://github.com/vaughnwalters"><i class="fa fa-github"> </i></a>
 
+            <a href="https://www.linkedin.com/in/vaughnwalters/"><i class="fa fa-linkedin"></i></a>
 
+            <a href="https://open.spotify.com/artist/3d9s7s6CPO2s9dRfWng6F1?si=-C8WL95CQ_6v6oXpRaVImQ"><i class="fa fa-spotify"></i></a>
+
+            <a href="https://www.instagram.com/cheataqua/"><i class="fa fa-instagram"></i></a>
 
         </div>
         <!-- /.col-lg-12 -->
--- includes/header.php
+++ includes/header.php
@@ -21,6 +21,10 @@
 
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png">
 
+<!--    font awesome-->
+    <link href="admin/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
