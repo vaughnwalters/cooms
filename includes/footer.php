507321efe32fbd97f7e7670901498d6b2db05719
Vaughn Walters <vaughnwalters@gmail.com>
2018-08-28 11:18:39 -0500

    updating jquery to 3.0.0

--- includes/footer.php
+++ includes/footer.php
@@ -29,10 +29,9 @@
 <!-- /.container -->
 
 <!-- jQuery -->
-<script
-        src="https://code.jquery.com/jquery-1.11.1.min.js"
-        integrity="sha256-VAvG3sHdS5LqTT+5A/aeq/bZGa/Uj04xKxY8KM/w9EE="
-        crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.0.0.min.js
+
+"></script>
 
 <!-- Bootstrap Core JavaScript -->
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
